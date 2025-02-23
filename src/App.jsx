dc87ad4cc1d07ac31b9f33a6e24392471603f95a
quiet-cmd <max.zh.petrov@gmail.com>
2025-02-23 12:26:15 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { Routes, Route, NavLink, Link, useParams } from "react-router-dom"
+import characters  from './assets/characters.json'
+import episodes from './assets/episode.json'
+import locations from './assets/location.json'
+
+const ROUTES = {
+  'main': '/',
+  'characters': '/characters',
+  'locations': '/locations',
+  'episodes': '/episodes',
+  'character': (id) => `${ROUTES.characters}/${id}`,
+  'location': (id) => `${ROUTES.location}/${id}`,
+  'episode': (id) => `${ROUTES.episodes}/${id}`,
+}
+
+function ListItem() {
+  const { id } = useParams();
+  const character = characters[Number(id) - 1];
+
+  if (!character) {
+    return <p>404</p>;
+  }
+
+  const characterEntries = Object.entries(character);
+  return (
+    <ul>
+      {characterEntries.map(([key, value]) => (<li key={key}>{value}</li>))}
+    </ul>
+  );
+}
+
+function App() {
+  const renderList = (list) => (
+    <ul>
+      {list.map(({id, name}) => (<li key={id}><Link to={`./${id}`}>{name}</Link></li>))}
+    </ul>
+  )
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li><NavLink to='/'>main</NavLink></li>
+          <li><NavLink to='/characters'>characters</NavLink></li>
+          <li><NavLink to='/locations'>locations</NavLink></li>
+          <li><NavLink to='/episodes'>episodes</NavLink></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<p>main page</p>} />
+        <Route path='/characters' element={renderList(characters)} />
+        <Route path='/characters/:id' element={<ListItem />} />
+        <Route path='/locations' element={renderList(locations)} />
+        <Route path='/locations/:id' element={<ListItem />} />
+        <Route path='/episodes' element={renderList(episodes)} />
+        <Route path='/episodes/:id' element={<ListItem />} />
+        <Route path='*' element={<p>something wrong</p>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
